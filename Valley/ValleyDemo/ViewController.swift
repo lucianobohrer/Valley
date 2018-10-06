47153b0a74f640634a4086dddfa6de79cfb19904
Luciano Bohrer <luciano_acosta@sicredi.com.br>
2018-10-06 15:53:14 -0300

    Project Demo setup

---
+++ Valley/ValleyDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ValleyDemo
+//
+//  Created by Luciano Bohrer on 06/10/18.
+//  Copyright © 2018 Luciano Bohrer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
