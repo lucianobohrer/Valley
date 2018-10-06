ea7a6462c830ca24951e06df15ef083e224fb091
Luciano Bohrer <luciano_acosta@sicredi.com.br>
2018-10-06 15:41:14 -0300

    Project creation

---
+++ Valley/Valley/Application/Valley.h
@@ -0,0 +1,19 @@
+//
+//  Valley.h
+//  Valley
+//
+//  Created by Luciano Bohrer on 06/10/18.
+//  Copyright © 2018 Luciano Bohrer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Valley.
+FOUNDATION_EXPORT double ValleyVersionNumber;
+
+//! Project version string for Valley.
+FOUNDATION_EXPORT const unsigned char ValleyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Valley/PublicHeader.h>
+
+
